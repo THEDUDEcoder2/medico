f8374b314e765114ed69c078e051f9b4d67627a9
THEDUDEcoder2 <toledo_louis@hotmail.com>
2025-03-27 17:57:29 -0700

    unos cambios al menu y nueva ventana junto con controller

--- src/main/java/com/example/medico/consultaController.java
+++ src/main/java/com/example/medico/consultaController.java
@@ -5,8 +5,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.MenuButton;
-import javafx.scene.control.MenuItem;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,39 +14,43 @@ import java.io.IOException;
 public class consultaController {
 
     @FXML
-    private MenuButton MBespecialidad;
-
-    private static String especialidadSeleccionada;
+    private Button BCrearPerfil;
 
     @FXML
-    public void initialize() {
-
-        for (MenuItem item : MBespecialidad.getItems()) {
-            item.setOnAction(event -> {
-                especialidadSeleccionada = item.getText();
-                MBespecialidad.setText(especialidadSeleccionada);
-            });
-        }
-    }
+    private Button BIniciarSesion;
 
     @FXML
-    private void elegirEspecialidad(ActionEvent event) {
-        MenuItem item = (MenuItem) event.getSource();
-        especialidadSeleccionada = item.getText();
-        MBespecialidad.setText(especialidadSeleccionada);
-    }
+    private Button Bpacientes;
 
+    @FXML
+    private TextField TxtCedulaProfesional;
+
+    @FXML
+    private TextField TxtContraseña;
+
+    @FXML
+    void IniciarSesion(ActionEvent event) {
+
+    }
     @FXML
     private void segundaVentana(ActionEvent event) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("segunda ventana.fxml"));
         Parent root = loader.load();
 
 
-        segundaVentanaController controller = loader.getController();
-        controller.setEspecialidad(especialidadSeleccionada);
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+    public void Doctor(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("crear perfil.fxml"));
+        Parent root = loader.load();
+
 
         Stage stage = new Stage();
         stage.setScene(new Scene(root));
         stage.show();
     }
+
 }
